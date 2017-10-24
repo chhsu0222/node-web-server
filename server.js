e2c229aae3cdaaccf81d0b47c111a9b99186dae1
chhsu_startek <ch@startek-eng.com>
2017-10-24 17:34:05 +0800

    Express middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express(); // create a new express app
 
@@ -20,6 +21,22 @@ an instance of the app object by using the app.use()
 express.static, a built-in middleware of Express,
 serves static assets such as HTML files, images, and so on.
 */
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
